5241f85af943341afb0f294c748320c8b2f14834
Kacial <rzepkaa.maciej@gmail.com>
2024-10-16 19:45:22 +0200

    fix unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import main from "../app/content/main.png";
-import main_content from "../app/content/main inside.png";
 import { Footer } from "@/app/components/Footer";
 import { TopSection } from "@/app/components/TopSection";
 import { TopToolbar } from "@/app/components/TopToolbar";
@@ -9,11 +6,9 @@ import { FirstChapter } from "@/app/components/FirstChapter";
 export default function Home() {
   return (
     <div className={"w-full"}>
-      {/*<Image src={main} alt={"general"}/>*/}
       <TopToolbar />
       <TopSection />
       <FirstChapter />
-      {/*<Image src={main_content} alt={"inside_content"} />*/}
       <Footer />
     </div>
   );
